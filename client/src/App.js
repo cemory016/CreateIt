3478a9f27965c4ca31cf8f65e32433fc8477a82d
Carmen <cemory016@gmail.com>
2018-04-09 16:47:53 -0400

    basic navbar with title on landing page

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,34 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import React, {Component} from "react";
+import {BrowserRouter as Router, Route, Switch, Link} from "react-router-dom";
+import NavBar from "./components/NavBar";
+import "./App.css";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <Router>
+              <div>
+              <NavBar/>
+              <Switch>
+                <div className="App">
+
+                    <div>
+                        <h1>Craft It!</h1>
+                        <div>
+                            <div><Link to="/">Crafts</Link></div>
+                        </div>
+                    </div>
+
+                    {/* <Route exact path="/" component={Craft}/>
+                    <Route path="/artist/:id" component={User}/> */}
+
+                </div>
+                </Switch>
+                </div>
+            </Router>
+        );
+    }
 }
 
 export default App;
