837e6006abaae618d5a13a88903edb9d52074a8b
Carmen <cemory016@gmail.com>
2018-04-11 18:53:54 -0400

    trying to get my buttons, forms, and create to work

--- client/src/components/CraftComment.js
+++ client/src/components/CraftComment.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import { Link } from 'react-router-dom'
-import { Image, Button, Card } from 'semantic-ui-react'
+import { Link } from 'react-router-dom';
+import EditCommentsForm from './EditCommentsForm';
+import { Image, Button, Card, Form, Input, TextArea } from 'semantic-ui-react';
 
 class CraftComment extends Component {
     constructor() {
@@ -21,7 +22,7 @@ class CraftComment extends Component {
     getUserAndCraftsAndCommentsData = async (userId, craftId) => {
         try {
             const userResponse = await axios.get(`/api/users/${userId}`)
-       
+
             const craftsResponse = await axios.get(`/api/users/${userId}/crafts/${craftId}`)
 
             const commentsResponse = await axios.get(`/api/users/${userId}/crafts/${craftId}/comments`)
@@ -31,22 +32,13 @@ class CraftComment extends Component {
                 craft: craftsResponse.data.craft,
                 comments: commentsResponse.data.comments
             });
-           
+
         }
         catch (error) {
             console.log(error)
-            await this.setState({error: error.message});
+            await this.setState({ error: error.message });
             return error.message
         }
-   // handleSubmit = async e => {
-        //     e.preventDefault()
-        //     const commentId = this.state.comment.id
-        //     const artistUpdate = { ...this.state.comment }
-        //     await axios.patch(`api/users/${userId}/crafts/${craftId}/comments`, artistUpdate)
-        //     this.toggleShowEdit()
-        //     await this.getSingleArtist()
-        //   };
-
     };
 
 
@@ -56,26 +48,46 @@ class CraftComment extends Component {
         const commentsId = this.props.comments_id
         await axios.delete(`/api/users/${this.props.userId}/crafts/${this.props.craftId}/comments/${id}`)
         this.props.history.push('/')
-      };
+    };
+
+    // handleSubmit = async e => {
+    //     e.preventDefault()
+    //     const userId = this.props.userId;
+    //     const craftId = this.props.craftId;
+    //     const commentId = this.props.comment_id
+    //     const commentNew = { ...this.state.comment }
+    //     await axios.patch(`api/users/${userId}/crafts/${craftId}/comments`, commentNew)
+    //     // this.toggleShowEdit()
+    //     // await this.getSingleArtist()
+    // };
+
+
     render() {
- 
+
         // if (this.state.error){
         //     return <div>{this.state.error}</div>   
         // }
-        
+
         return (
             <div>
                 <h1>Craft Comments</h1>
-                <Button>New Comment</Button>
+                {/* {this.state.comments} */}
+                {/* <Button onClick={() => this.newComment(comments.id)}>New Comment</Button> */}
+                
                 {this.state.comments.map(comment => (
                     <div key={comment.id}>
-                    {comment.title}
-                    {comment.text}
-                    <Button onClick={()=>this.deleteComment(comment.id)}>Delete</Button>
+                        {comment.title}
+                        {comment.text}
+                        <Button onClick={() => this.deleteComment(comment.id)}>Delete</Button>
+                        <Button onClick={() => this.editComment(comment.id)}>Edit Comment</Button>
                     </div>
                 ))}
                 <Button>Edit</Button>
-                
+                {/* <EditCommentsForm 
+                handleChange={this.handleChange}
+                handleSubmit={this.handleSubmit}
+                comments={this.props.comments}
+                /> */}
             </div>
         );
     }
